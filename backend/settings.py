819961c01ce9acbfc141b04ccfc1049a25cb5ce2
Udayan Gupta <62198309+UdynGP@users.noreply.github.com>
2022-01-19 12:43:58 +0530

    Force SSL Redirect

--- backend/settings.py
+++ backend/settings.py
@@ -205,4 +205,6 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'gadgethub-bucket'
 
 if os.getcwd() == '/app':
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
     DEBUG = False
